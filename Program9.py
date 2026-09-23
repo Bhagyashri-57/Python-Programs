4722b6c4af200fe159e710986542b3c475240ad4
Bhagyashri-57 <patilcbhagya@gmail.com>
2026-09-23 22:07:40 +0530

    PythonPrograms

---
+++ Program9.py
@@ -0,0 +1,11 @@
+marks=int(input("Enter the Marks: "))
+if marks>90 and marks<=100:
+    print("Obtained Grade A")
+elif marks>80 and marks<=90:
+    print("Obtained Grade B")
+elif marks>70 and marks<=80:
+    print("obtained Grade C")
+elif marks>60 and marks<=70:
+    print("Obtained Grade D")
+else:
+    print("Obtained Grade E") 
